6863e32006cd33c1078da2bd41063f2a5d06de92
Olga <avramenkoos@mail.ru>
2019-12-01 21:08:33 +0300

    Created GroupHelper

--- fixture/application.py
+++ fixture/application.py
@@ -1,45 +1,19 @@
 from selenium.webdriver.firefox.webdriver import WebDriver
 from selenium.webdriver.support.ui import Select
 from fixture.session import SessionHelper
+from fixture.group import GroupHelper
 
 class Application:
     def __init__(self):
         self.wd = WebDriver()#launch browser
         self.wd.implicitly_wait(60)
         self.session = SessionHelper(self) #allow to use wd in session.py
+        self.group = GroupHelper(self)
 
     def open_homepage(self):
         # open homepage
         wd = self.wd
         wd.get("http://localhost/addressbook/")
-
-    def open_groups_page(self):
-        # open groups page
-        wd = self.wd
-        wd.find_element_by_link_text("groups").click()
-
-    def create_group(self, group):
-        # init groups creation
-        wd = self.wd
-        self.open_groups_page()
-        wd.find_element_by_name("new").click()
-        # fill group form
-        wd.find_element_by_name("group_name").click()
-        wd.find_element_by_name("group_name").clear()
-        wd.find_element_by_name("group_name").send_keys(group.name)
-        wd.find_element_by_name("group_header").click()
-        wd.find_element_by_name("group_header").clear()
-        wd.find_element_by_name("group_header").send_keys(group.header)
-        wd.find_element_by_name("group_footer").clear()
-        wd.find_element_by_name("group_footer").send_keys(group.footer)
-        # submit group creation
-        wd.find_element_by_name("submit").click()
-        self.return_to_groups_page()
-
-    def return_to_groups_page(self):
-        # return to group page
-        wd = self.wd
-        wd.find_element_by_link_text("group page").click()
 
     def destroy(self):
         self.wd.quit()
--- test/test_add_group.py
+++ test/test_add_group.py
@@ -11,10 +11,10 @@ def app(request):
 def test_add_group(app):
     app.session.login(username="admin",
                    password="secret")  # don't change because these fields will not changed(almost 100% only 2 parametrs)
-    app.create_group(Group(name="training", header="training", footer="training"))
+    app.group.create(Group(name="training", header="training", footer="training"))
     app.session.logout()
 
 def test_add_empty_group(app):
     app.session.login(username="admin", password="secret")
-    app.create_group(Group(name="", header="", footer=""))
+    app.group.create(Group(name="", header="", footer=""))
     app.session.logout()

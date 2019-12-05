b16a6b96c3262c98834c65eec77cac495128c608
Olga <avramenkoos@mail.ru>
2019-12-05 20:15:44 +0300

    Added test_modify_group

--- fixture/group.py
+++ fixture/group.py
@@ -38,3 +38,15 @@ class GroupHelper:
         wd.find_element_by_name("selected[]").click()
         wd.find_element_by_name("delete").click()
         self.return_to_groups_page()
+
+    def modify_first(self):
+        wd = self.app.wd
+        self.open_groups_page()
+        wd.find_element_by_name("selected[]").click()
+        wd.find_element_by_name("edit").click()
+        wd.find_element_by_name("group_name").click()
+        wd.find_element_by_name("group_name").clear()
+        wd.find_element_by_name("group_name").send_keys("modify")
+        wd.find_element_by_name("update").click()
+        self.return_to_groups_page()
+

ccda7e63883819ef16182b64be8226c1e68c2e0f
Olga Avramenko <avramenkoos@mail.ru>
2020-01-22 09:51:51 +0300

    Final config targetjson (abspath)

--- conftest.py
+++ conftest.py
@@ -1,7 +1,9 @@
 # Create common fixture
 from fixture.application import Application  # group.py - package group, Group - module
-import pytest
 import json
+import os.path
+import pytest
+
 
 fixture = None
 target = None
@@ -13,8 +15,9 @@ def app(request):
     global target
     browser = request.config.getoption("--browser")
     if target is None:
-        with open(request.config.getoption("--target")) as config_file:
-            target = json.load(config_file)
+        config_file = os.path.join(os.path.dirname(os.path.abspath(__file__)), request.config.getoption("--target"))
+        with open(config_file) as f:
+            target = json.load(f)
     if fixture is None or not fixture.is_valid():  # check that fixture not exists
         fixture = Application(browser=browser, baseUrl=target['baseUrl'])
     fixture.session.ensure_login(username=target['username'], password=target['password'])  # make it ones for all tests

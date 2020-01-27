49b8fb02e510cd6f1dcb544a5d8ee2db54fe40af
Olga Avramenko <avramenkoos@mail.ru>
2020-01-27 09:14:48 +0300

    Added check_ui for add group

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -1,11 +1,13 @@
 from model.group import Group
 
 
-def test_add_group(app, db, json_groups): # json_groups for output in .py
+def test_add_group(app, db, json_groups, check_ui): # json_groups for output in .py
     group = json_groups
     old_groups = db.get_group_list() # get info from db
     app.group.create(group)
     new_groups = db.get_group_list()
     old_groups.append(group)
     assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
+    if check_ui: # flag to on/off assert
+        assert sorted(new_groups, key=Group.id_or_max) == sorted(app.group.get_list(), key=Group.id_or_max)
 
